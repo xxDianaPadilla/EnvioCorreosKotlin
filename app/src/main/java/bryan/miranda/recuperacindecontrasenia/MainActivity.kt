38908079810702e32ffb0ce0190196718b502df5
Bryan <bryan_miranda@ricaldone.edu.sv>
2024-06-24 22:17:16 -0600

    initial commit

---
+++ app/src/main/java/bryan/miranda/recuperacindecontrasenia/MainActivity.kt
@@ -0,0 +1,34 @@
+package bryan.miranda.recuperacindecontrasenia
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
+
+        btnEnviar.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                enviarCorreo("exequiel.miranda245@gmail.com", "Recuperación de contraseña", "Hola")
+
+            }
+        }
+
+    }
+}
